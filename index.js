0eccacb773ca9e4f33380425885b08fe72aee786
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-06-03 23:15:02 -0300

    stdin

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const questions = [
 ]
 
 const ask = (index = 0) => {
-    process.stdout.write(questions[index] + "\n")
+    process.stdout.write("\n\n" + questions[index] + " > ")
 }
 
 ask()
+
+const answers = []
+
+process.stdin.on('data', data => {
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit()
+    }
+})
